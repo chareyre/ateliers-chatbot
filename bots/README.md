439229aede2eebb95bb19dbc89d90382537ceba8
Léon Lenclos <leon.lenclos@gmail.com>
2019-03-05 09:48:39 +0100

    bots dir

---
+++ bots/README.md
@@ -0,0 +1,3 @@
+# Chatbots créés en atelier
+
+- [Le chatbot de Chateauponsac](gdr1)
---
+++ bots/gdr1/riveList.js
@@ -0,0 +1,15 @@
+// This array must contain the list of all the rive to include.
+
+const riveList = [
+    'brain/louis-timbal.rive',
+    'brain/gouts.rive',
+    'brain/sorties-scolaires.rive',
+    'brain/booba-kaaris.rive',
+    'brain/blagues.rive',
+    'brain/musique.rive',
+    'brain/jeux-videos.rive',
+    'brain/menu.rive',
+    
+    'brain/special.rive'
+];
+
