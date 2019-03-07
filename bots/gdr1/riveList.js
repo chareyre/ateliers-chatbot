e87baf8c1693de99d91b1fc0e8e027e33558eac2
Léon Lenclos <leon.lenclos@gmail.com>
2019-03-07 12:34:28 +0100

    session 1 3e1 chateauponsac

--- bots/gdr1/riveList.js
+++ bots/gdr1/riveList.js
@@ -1,19 +1,28 @@
 // This array must contain the list of all the rive to include.
 
 const riveList = [
-    'brain/louis-timbal.rive',
-    'brain/gouts.rive',
-    'brain/sorties-scolaires.rive',
-    'brain/booba-kaaris.rive',
-    'brain/blagues.rive',
-    'brain/musique.rive',
-    'brain/jeux-videos.rive',
-    'brain/renvoyer-a-la-question.rive',
-    'brain/menu.rive',
-    'brain/cvc.rive',
-    'brain/tableaux.rive',
-    'brain/fourre-tout.rive',
-    
-    'brain/special.rive'
+	"brain/musique.rive",
+	"brain/louis-timbal.rive",
+	"brain/jeux-videos.rive",
+	"brain/sentiments.rive",
+	"brain/sport-quil-fait.rive",
+	"brain/special.rive",
+	"brain/tableaux.rive",
+	"brain/actualite.rive",
+	"brain/blagues.rive",
+	"brain/netflix.rive",
+	"brain/sorties-scolaires.rive",
+	"brain/sport-general.rive",
+	"brain/gouts.rive",
+	"brain/nourriture.rive",
+	"brain/booba-kaaris.rive",
+	"brain/fourre-tout.rive",
+	"brain/mode.rive",
+	"brain/menu.rive",
+	"brain/cvc.rive",
+	"brain/sport-mecanique.rive",
+	"brain/salutations.rive",
+	"brain/renvoyer-a-la-question.rive"
 ];
+
 
