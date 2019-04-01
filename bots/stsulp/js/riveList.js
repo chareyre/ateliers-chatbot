79951720f2efbcad40a7d7654c305d3c610bfcf2
Léon Lenclos <leon.lenclos@gmail.com>
2019-04-01 09:12:48 +0200

    vite vite git push

--- bots/stsulp/js/riveList.js
+++ bots/stsulp/js/riveList.js
@@ -1,38 +1,7 @@
 // This array must contain the list of all the rive to include.
 
 const riveList = [
-"brain/musique.rive",
-"brain/meteo.rive",
-"brain/louis-timbal.rive",
-"brain/jeux-videos.rive",
-"brain/sentiments.rive",
-"brain/sport-quil-fait.rive",
-"brain/special.rive",
-"brain/tableaux.rive",
-"brain/actualite.rive",
-"brain/blagues.rive",
-"brain/netflix.rive",
-"brain/sorties-scolaires.rive",
-"brain/se-presenter.rive",
-"brain/sport-general.rive",
-"brain/gouts.rive",
-"brain/dormir.rive",
-"brain/insultes.rive",
-"brain/nourriture.rive",
-"brain/graines-de-rue.rive",
-"brain/booba-kaaris.rive",
-"brain/fourre-tout.rive",
-"brain/ennui.rive",
-"brain/raconte-ta-journee.rive",
-"brain/matiere-profs.rive",
-"brain/mode.rive",
-"brain/menu.rive",
-"brain/cvc.rive",
-"brain/sport-mecanique.rive",
-"brain/salutations.rive",
-"brain/renvoyer-a-la-question.rive",
-"brain/reseauxsociaux.rive",
-"brain/sa-vie.rive"
+"brain/special.rive"
 ];
 
 
