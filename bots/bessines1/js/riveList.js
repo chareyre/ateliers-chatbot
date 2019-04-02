79833a5a4f4edeba1d9eb06baba5e26fe487de0c
Léon Lenclos <leon.lenclos@gmail.com>
2019-04-02 12:39:44 +0200

    am

--- bots/bessines1/js/riveList.js
+++ bots/bessines1/js/riveList.js
@@ -2,21 +2,21 @@
 
 const riveList = [
 "brain/special.rive",
-"/brain/musique.rive",
-"/brain/animaux.rive",
-"/brain/special.rive",
-"/brain/ecologie.rive",
-"/brain/voyages.rive",
-"/brain/sport.rive",
-"/brain/arts.rive",
-"/brain/internet.rive",
-"/brain/gouts.rive",
-"/brain/betise.rive",
-"/brain/jeuxvideos.rive",
-"/brain/nourriture.rive",
-"/brain/salutations.rive",
-"/brain/cinema.rive",
-"/brain/presentation.rive"
+"brain/musique.rive",
+"brain/animaux.rive",
+"brain/special.rive",
+"brain/ecologie.rive",
+"brain/voyages.rive",
+"brain/sport.rive",
+"brain/arts.rive",
+"brain/internet.rive",
+"brain/gouts.rive",
+"brain/betise.rive",
+"brain/jeuxvideos.rive",
+"brain/nourriture.rive",
+"brain/salutations.rive",
+"brain/cinema.rive",
+"brain/presentation.rive"
 ];
 
 
